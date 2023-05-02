75ae6fd32465dad497b795127e2f7d2bfc5061bf
SlimeMite <112862802+SlimeMite@users.noreply.github.com>
2023-05-02 16:12:23 +0200

    Made it better
    No Wall-Wraparound,
    limited Velocity,
    better Wallrunning,
    prob. other stuff I'm forgetting

--- Neo_Velocity/Assets/PlayerScripts/PlayerScript.cs
+++ Neo_Velocity/Assets/PlayerScripts/PlayerScript.cs
@@ -1,11 +1,24 @@
 using JetBrains.Rider.Unity.Editor;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
+
+// Wall Detect+
+// PastWalls List
+// set delay till again touchable
+// reset delay if touch in delay
+
+// Wallrunning
+// WallCollider.GetComponent<Collider>().ClosestPoint()
+// Teleport next to wall
 
 public class PlayerScript : MonoBehaviour
 {
+    #region Field Declaration
+
     [SerializeField] GameObject PlayerCamera;
     [SerializeField] GameObject WallCollider;
     [SerializeField] GameObject GroundCollider;
@@ -13,7 +26,8 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] float Gravity;
     [SerializeField] float LookSensitivity;
     [SerializeField] float AccelerationSpeed;
-    [SerializeField] float MaxMoveSpeed;
+    [SerializeField] float ApproachingCooeficient = 60000; // ChatGPT too dumb for this
+    [SerializeField] float MaxMoveSpeed = 10000;
     [SerializeField] float TerminalVelocity;
     [SerializeField] float GroundMercyTime;
     [SerializeField] float JumpDelay;
@@ -21,8 +35,13 @@ public class PlayerScript : MonoBehaviour
     [SerializeField] float MaxJumpVelocity;
     [SerializeField] float WallStickTime;
     [SerializeField] float WallJumpDelay;
-    [SerializeField] float FrictionDelay;
-    [SerializeField] float FrictionForce;
+    [SerializeField] float WallRunDistanceFromWall = 0.51f; // How far a wall teleports you (Bean is r=0.5, so 0.51 works well)
+    [SerializeField] float FrictionDelay; // Doesn't actually do anything
+    [SerializeField] float FrictionForce; // Decelerate movement
+    [SerializeField] float FrictionCompensation; // Accelerate normal movement by apllied Friction
+    [SerializeField] float WallRunDelay; // If you fall of a Wall, how long until you can run again
+    [SerializeField] float WallJumpForgetDelay; // How long you have to stick to a wall, for it to forget your initial touchspeed
+    [SerializeField] float FOV;
 
     new Rigidbody rigidbody;
     new GameObject camera;
@@ -37,14 +56,17 @@ public class PlayerScript : MonoBehaviour
 
     float LastGroundedTime;
     float LastJumpTime;
-    bool IsGrounded; // Wink
+    bool IsGrounded; // Wink   <- Why did I write this?
     GameObject CurrentWall;
     GameObject LastWall;
     float LastWallTouch;
     float LastWallJump;
     Vector3 LastVelocityAtTouch;
-    bool WallTouched;
     bool WallRunning;
+    float LastWallRun;
+    float WallJumpForgetTime;
+
+    #endregion
 
     void Start()
     {
@@ -65,7 +87,16 @@ public class PlayerScript : MonoBehaviour
         LastJumpTime = 0f;
         LastWallTouch = 0f;
         LastWallJump = 0f;
+        LastWallRun = 0f;
     }
+
+    #region MagnitudeInDirection
+    /// <summary>
+    /// Outputs the Magnitude of the Magnitude Vector, after being projected on the DirectionGiver
+    /// </summary>
+    /// <param name="DirectionGiver"></param>
+    /// <param name="Magnitude"></param>
+    /// <returns></returns>
     float MagnitudeInDirection(Vector3 DirectionGiver, Vector3 Magnitude)
     {
         Vector3 Projected = Vector3.Project(Magnitude, DirectionGiver);
@@ -73,14 +104,23 @@ public class PlayerScript : MonoBehaviour
             return Projected.magnitude;
         return -Projected.magnitude;
     }
+    #endregion
 
     void Update()
     {
+        #region Advance Timers
         LastGroundedTime += Time.deltaTime;
         LastJumpTime += Time.deltaTime;
         LastWallTouch += Time.deltaTime;
         LastWallJump += Time.deltaTime;
+        LastWallRun += Time.deltaTime;
+        WallJumpForgetTime += Time.deltaTime;
+        #endregion
 
+        // Floor and Wall
+        #region Collision Handling
+
+        // Checks if collided with a Floor
         if (OtherFloor)
         {
             if (velocity.y < 0)
@@ -89,8 +129,12 @@ public class PlayerScript : MonoBehaviour
                 velocity.y = 0;
             }
         }
+
+        // Checks if collided with a Wall (that isn't the Floor)
         if (OtherWall && OtherWall != OtherFloor)
         {
+            if (LastWallJump > WallJumpDelay && LastWallRun > WallRunDelay)
+                WallRunning = true;
             Vector3 WallAwayVector;
             Vector3 ParallelToWall;
 
@@ -103,7 +147,7 @@ public class PlayerScript : MonoBehaviour
                 LastVelocityAtTouch = velocity;
                 LastWallTouch = 0f;
                 CurrentWall = OtherWall.gameObject;
-                WallTouched = true;
+                WallJumpForgetTime = 0f;
             }
             else if (CurrentWall == OtherWall.gameObject)
             {
@@ -115,8 +159,11 @@ public class PlayerScript : MonoBehaviour
                 LastWallTouch = 0f;
                 LastWall = CurrentWall;
                 CurrentWall = OtherWall.gameObject;
-                WallTouched = true;
+                WallJumpForgetTime = 0f;
             }
+
+            // Aligns Momentum in direction of the wall, if facing towards the wall
+            #region AlignMomentumToWall
             WallAwayVector = transform.position - OtherWall.gameObject.GetComponent<Collider>().ClosestPoint(transform.position);
             if (Vector3.Angle(velocity, -WallAwayVector) < 90)
             {
@@ -126,24 +173,34 @@ public class PlayerScript : MonoBehaviour
                 velocity.x = planeAligned.x;
                 velocity.z = planeAligned.z;
             }
+            #endregion
         }
+
         OtherFloor = null;
         OtherWall = null;
+        #endregion
 
         IsGrounded = LastGroundedTime < GroundMercyTime;
+
         if (LastWallTouch > WallStickTime)
         {
             CurrentWall = null;
             LastWall = null;
-        }
+            WallRunning = false;
+        } // Reset the touched walls
 
-        float MouseX = Input.GetAxis("Mouse X") * LookSensitivity * Time.deltaTime;
+        // Axis - Sensi: 0,1
+        float MouseX = Input.GetAxis("Mouse X") * LookSensitivity * Time.deltaTime; // <- Time.deltaTime might be wrong, depending on how Mouse Axis work
         float MouseY = -Input.GetAxis("Mouse Y") * LookSensitivity * Time.deltaTime;
 
+        // Mouse Movement doesn't line up with Desktop Movement, don't know why
+
+        #region General Rotation
         RotationEuler.y += MouseX;
         Rotation.eulerAngles = RotationEuler;
         transform.rotation = Rotation;
-
+        #endregion
+        #region Camera Rotation
         CameraRotationEuler.x += MouseY;
         if (CameraRotationEuler.x > 90)
             CameraRotationEuler.x = 90;
@@ -151,40 +208,76 @@ public class PlayerScript : MonoBehaviour
             CameraRotationEuler.x = -90;
         CameraRotation.eulerAngles = CameraRotationEuler;
         camera.transform.localRotation = CameraRotation;
+        #endregion
 
         velocity.y += Gravity * Time.deltaTime;
 
+        #region PlaneMovement Vector
+        // for Vertical and Horizontal set:
+        // Gravity: 100
+        // Dead: 0.001
+        // Sensitivity: 15
         float Vertical = Input.GetAxis("Vertical");
         float Horizontal = Input.GetAxis("Horizontal");
         Vector3 PlaneMovement = new Vector3(Horizontal, 0, Vertical).normalized;
         PlaneMovement *= AccelerationSpeed * Time.deltaTime;
         PlaneMovement = Quaternion.AngleAxis(RotationEuler.y, Vector3.up) * PlaneMovement;
+        #endregion
 
         float MagnitudeInMovement = MagnitudeInDirection(PlaneMovement, velocity);
 
-        if (IsGrounded) // Friction
+        #region Friction
+        if (IsGrounded)
         {
+            Vector3 flatVelocity = new Vector3(velocity.x, 0, velocity.z);
             if (PlaneMovement.magnitude < 0.01)
             {
-                Vector3 flatVelocity = new Vector3(velocity.x, 0, velocity.z);
                 velocity -= flatVelocity * FrictionForce * Time.deltaTime;
             }
             else
             {
-                Vector3 flatVelocity = new Vector3(velocity.x, 0, velocity.z);
                 float flatDirection = Vector3.Angle(PlaneMovement, flatVelocity) / 180;
-                flatVelocity = flatVelocity * flatDirection * FrictionForce * Time.deltaTime;
-                velocity -= flatVelocity;
+                velocity -= flatVelocity * flatDirection * FrictionForce * Time.deltaTime;
+                PlaneMovement *= 1 + flatDirection / 2 * FrictionCompensation;
             }
         }
+        #endregion
 
+        #region Apply Movement
         if (MagnitudeInMovement + PlaneMovement.magnitude < MaxMoveSpeed)
             velocity += PlaneMovement;
         else if (MagnitudeInMovement < MaxMoveSpeed)
             velocity += PlaneMovement.normalized * (MaxMoveSpeed - MagnitudeInMovement);
+        #endregion
 
-        float Space = Input.GetAxis("Jump");
-        if (Space > 0.5f)
+        if (WallRunning && CurrentWall != null)
+        {
+            LastWallRun = 0f;
+            Vector3 WallAwayVector = transform.position - CurrentWall.GetComponent<Collider>().ClosestPoint(transform.position);
+            float AngleFromWallAndMovement = Vector3.Angle(PlaneMovement, WallAwayVector);
+            float AngleFromWallAndVelocity = Vector3.Angle(new Vector3(velocity.x, 0, velocity.z), WallAwayVector);
+            if (AngleFromWallAndMovement < 45 && PlaneMovement.magnitude > 0.01)
+            {
+                WallRunning = false;
+            }
+            else if (AngleFromWallAndVelocity < 20)
+            {
+                WallRunning = false;
+            }
+            else if (WallAwayVector.magnitude > WallRunDistanceFromWall)
+            {
+                WallAwayVector = WallAwayVector / WallAwayVector.magnitude * (WallAwayVector.magnitude - WallRunDistanceFromWall);
+                transform.position -= WallAwayVector;
+            }
+            velocity.y = 0;
+        } // everything wall
+
+        if (WallJumpForgetTime > WallJumpForgetDelay)
+        {
+            LastVelocityAtTouch = velocity;
+        }// Reset Walljump reflexion-direction to velocity
+
+        if (Input.GetKey(KeyCode.Space))
         {
             if (CurrentWall != null && LastWallJump > WallJumpDelay && LastJumpTime > JumpDelay && !IsGrounded)
             {
@@ -195,6 +288,7 @@ public class PlayerScript : MonoBehaviour
                 newVelocity = newVelocity - 2 * Vector3.Dot(newVelocity, WallAwayVector) * WallAwayVector;
                 newVelocity.y = velocity.y;
                 velocity = newVelocity;
+                velocity += WallAwayVector * JumpForce;
                 if (velocity.y < MaxJumpVelocity)
                 {
                     if (velocity.y + JumpForce < MaxJumpVelocity)
@@ -211,25 +305,26 @@ public class PlayerScript : MonoBehaviour
                 LastJumpTime = 0f;
                 LastGroundedTime = GroundMercyTime;
             }
-        }
+        } // Jump
+
         if (velocity.y < TerminalVelocity)
         {
             velocity.y = TerminalVelocity;
-        }
-
-        // Wallrunning
-        // WallCollider.GetComponent<Collider>().ClosestPoint()
-        // Teleport next to wall
+        } // Caps falling-speed at TerminalVelocity
 
         if (Input.GetKeyDown(KeyCode.F))
         {
             velocity = Vector3.zero;
             transform.position = new Vector3(-5, 2, 5);
-        }
-        rigidbody.velocity = velocity;
-        WallTouched = false;
+        } // Reset Character / Debug Purpose
+
+        Vector3 ActualVelocity = new Vector3(velocity.x, 0, velocity.z);
+        ActualVelocity = ActualVelocity.normalized * DecreasedReturn(ActualVelocity.magnitude);
+        rigidbody.velocity = new Vector3(ActualVelocity.x, velocity.y, ActualVelocity.z);
+        //camera.GetComponent<Camera>().fieldOfView = Mathf.Floor(FOV + FOV * Mathf.Pow(velocity.magnitude / 100, 0.1f));
     }
 
+    #region Save Collided
     void WallCollided(Collider other)
     {
         OtherWall = other;
@@ -237,5 +332,11 @@ public class PlayerScript : MonoBehaviour
     void GroundCollided(Collider other)
     {
         OtherFloor = other;
+    }
+    #endregion
+
+    float DecreasedReturn(float value)
+    {
+        return 200 - ApproachingCooeficient / (value + 300);
     }
 }

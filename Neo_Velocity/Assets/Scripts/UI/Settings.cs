69fde777015978743a235004dc59c31240408675
Adrian <95475192+eDifdaf@users.noreply.github.com>
2023-06-06 16:09:00 +0200

    different levels

---
+++ Neo_Velocity/Assets/Scripts/UI/Settings.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    [SerializeField] public GameObject player;
+
+    // Update is called once per frame
+    void Update()    {
+        //player.transform.rotation.y = player.transform.rotation.y + 0.5;
+    }
+}
